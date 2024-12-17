19dff25b2e7b5cf64c7825e7fe49c4c5f95e43b1
ArockiaShermila <sharmishar47@gmail.com>
2024-12-17 13:56:28 +0530

    code committed by shermila object detection

--- ML_files/object_detection.py
+++ ML_files/object_detection.py
@@ -12,7 +12,11 @@ from keras.optimizers import Adam
 from keras.models import Sequential
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
-
+from keras.layers import Dense,Dropout,Conv2D,MaxPooling2D,Flatten
+from keras.optimizers import Adam
+from keras.models import Sequential
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 train_dir= '../images/animals'
 val_dir= '../images/val_dir'
 
